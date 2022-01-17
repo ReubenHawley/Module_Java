8d3d5ed209d2364b1f2edb33f202f4a175c348b7
ReubenHawley <reubenhawley@protonmail.com>
2022-01-17 11:26:35 +0100

    Completed while loop
    - reimplement with for loop

--- src/com/reubenhawley/Main.java
+++ src/com/reubenhawley/Main.java
@@ -3,14 +3,22 @@ package com.reubenhawley;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-       Oefening drie= new Oefening();
-       System.out.println("Exercise 3A");
-       drie.drieAWhile(8);
+        // write your code here
+        Oefening drie = new Oefening();
+        System.out.println("Exercise 3A");
+        drie.drieAWhile(8);
 
         System.out.println("Exercise 3C");
-        drie.drieCWhile(8,20);
+        drie.drieCWhile(8, 20);
         System.out.println("Exercise 3D");
         drie.drieDWhile(8);
-       }
+        System.out.println("Exercise 3G");
+        drie.drieGWhile(4);
+        System.out.println("Exercise 3H");
+        drie.drieHWhile(6);
+        System.out.println("Exercise 3A: FOR");
+        drie.drieAFor(6);
+        System.out.println("Exercise 3C: FOR");
+        drie.drieCFor(6,3);
     }
+}
--- src/com/reubenhawley/Oefening.java
+++ src/com/reubenhawley/Oefening.java
@@ -2,51 +2,126 @@ package com.reubenhawley;
 
 public class Oefening {
 
-    public void drieAWhile(int size){
+    public void drieAWhile(int size) {
         int count = 0;
         int innerCount = 0;
-        while (count < size){
+        while (count < size) {
             // count the inner loop and update inner loop
-            if (innerCount < size -1){
+            if (innerCount < size - 1) {
                 System.out.print("*");
-            innerCount ++;
+                innerCount++;
             }
             // count the last value of the inner loop and update the outer loop after printing and reset inner loop to 0
-            else{
-                System.out.println("*");
-            count++;
-            innerCount = 0;
-            }
-        }
-    }
-    public void drieCWhile(int length, int breadth){
-        int count = 0;
-        int innerCount = 0;
-        while (count < length){
-            // count the inner loop and update inner loop
-            if (innerCount < breadth -1){
-                System.out.print("*");
-                innerCount ++;
-            }
-            // count the last value of the inner loop and update the outer loop after printing and reset inner loop to 0
-            else{
+            else {
                 System.out.println("*");
                 count++;
                 innerCount = 0;
             }
         }
     }
-    public void drieDWhile(int length){
+
+    public void drieCWhile(int length, int breadth) {
         int count = 0;
         int innerCount = 0;
-        while (count<length){
-            System.out.print("*");
-
-            if (innerCount<count){
-                System.out.println("*");
-                innerCount ++;
+        while (count < length) {
+            // count the inner loop and update inner loop
+            if (innerCount < breadth - 1) {
+                System.out.print("*");
+                innerCount++;
             }
-            count ++;
+            // count the last value of the inner loop and update the outer loop after printing and reset inner loop to 0
+            else {
+                System.out.println("*");
+                count++;
+                innerCount = 0;
+            }
+        }
+    }
+
+    public void drieDWhile(int length) {
+        int count = 0;
+        int innerCount = 0;
+        while (count < length) {
+            if (count == 0) {
+                System.out.println("*");
+            } else {
+                while (innerCount < count) {
+                    System.out.print("*");
+                    innerCount++;
+                }
+                System.out.println("*");
+                innerCount = 0;
+            }
+            count++;
+        }
+    }
+
+    public void drieGWhile(int length) {
+        length--;
+        int innerCount = length;
+        while (length != 0) {
+            while (innerCount != 0) {
+                System.out.print("*");
+                innerCount--;
+            }
+            System.out.println("*");
+            length--;
+            innerCount = length;
+
+        }
+        System.out.println("*");
+    }
+
+    public void drieHWhile(int length) {
+        if ((length % 2) != 0) {
+            int count = 0;
+            int innerCount = 0;
+            while (count < length) {
+                if (count == 0) {
+                    System.out.println("*");
+                } else {
+                    while (innerCount < count) {
+                        System.out.print("*");
+                        innerCount++;
+                    }
+                    System.out.println("*");
+                    innerCount = 0;
+                }
+                count++;
+            }
+            length--;
+            length--;
+            innerCount = length;
+            while (length != 0) {
+                while (innerCount != 0) {
+                    System.out.print("*");
+                    innerCount--;
+                }
+                System.out.println("*");
+                length--;
+                innerCount = length;
+
+            }
+            System.out.println("*");
+        }
+    }
+
+    public void drieAFor(int length) {
+        for (int i = 0; i < length-1; i++) {
+            for (int j = 0; j < length-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+
+    public void drieCFor(int length, int breadth) {
+        for (int i = 0; i < breadth-1; i++) {
+            for (int j = 0; j < length-1; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
         }
     }
 }
+
