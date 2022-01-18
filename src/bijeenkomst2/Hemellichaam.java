05855e7b7e34e1ad5aea0f246eef12e8f7711b58
ReubenHawley <reubenhawley@protonmail.com>
2022-01-18 14:08:25 +0100

    // Opdracht D
    - create attributes tempMin and tempMax
    - create setter for Temperatuur
    - create printTemperatuur method
    - call printTemperature method in main

--- src/bijeenkomst2/Hemellichaam.java
+++ src/bijeenkomst2/Hemellichaam.java
@@ -3,26 +3,50 @@ package bijeenkomst2;
 public class Hemellichaam {
     private String naam;
     private String type;
+    private int tempMin;
+    private int tempMax;
+
+    public String getNaam() {
+        return this.naam;
+    }
 
     public String getType() {
         return this.type;
     }
 
-    public String getNaam() {
-        return this.naam;
-    }
-    Hemellichaam(String naam, String type){
-        this.naam = naam;
+    private void setType(String type) {
         this.type = type;
     }
-    Hemellichaam(String naam){
+
+    public void setTemperatuur(int tempMin) {
+        this.tempMin = tempMin;
+    }
+
+    public void setTemperatuur(int temp, int tempMax) {
+        this.tempMin = temp;
+        this.tempMax = tempMax;
+    }
+
+    private void printTemperatuur() {
+        System.out.println("Het is minimaal "+tempMin+" graden, en maximaal "+ tempMax+" graden\n");
+    }
+
+    Hemellichaam(String naam, String type) {
+        new Hemellichaam(naam);
+        this.type = type;
+
+    }
+
+    Hemellichaam(String naam) {
         this.naam = naam;
         this.type = "onbekend";
+        this.tempMin = 0;
+        this.tempMax = 0;
     }
 
     @Override
     public String toString() {
-        return "Hemellichaam (" + type +')';
+        return naam + "(" + type + ")";
     }
 
     public static void main(String[] args) {
@@ -30,5 +54,21 @@ public class Hemellichaam {
         System.out.println("naam: " + hl1.getNaam());
         System.out.println("type: " + hl1.getType());
         System.out.println(hl1);
+
+        //opdracht D
+        Hemellichaam hl3 = new Hemellichaam("Pluto", "planeet");
+        System.out.println(hl3);
+        hl3.setType("dwergplaneet");
+        System.out.println(hl3);
+
+        //opdracht E
+        Hemellichaam hl4 = new Hemellichaam("Aarde", "planeet");
+        hl4.setTemperatuur(-89, 58);
+        hl4.printTemperatuur();
+
+
     }
+
+
+
 }
