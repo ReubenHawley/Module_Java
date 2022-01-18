915233155dec067580fb06b18909d61cef35a845
ReubenHawley <reubenhawley@protonmail.com>
2022-01-18 13:43:34 +0100

    // Opdracht A
    - create Hemellichaam
    - create attributes for name and type
    - create getters for name and type
    instantiate Hemellichaam in main method

---
+++ src/bijeenkomst2/Hemellichaam.java
@@ -0,0 +1,25 @@
+package bijeenkomst2;
+
+public class Hemellichaam {
+    private String naam;
+    private String type;
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getNaam() {
+        return this.naam;
+    }
+    Hemellichaam(String naam, String type){
+        this.naam = naam;
+        this.type = type;
+    }
+
+
+    public static void main(String[] args) {
+        Hemellichaam hl1 = new Hemellichaam("Mercurius", "planeet");
+        System.out.println("naam: " + hl1.getNaam());
+        System.out.println("type: " + hl1.getType());
+    }
+}
