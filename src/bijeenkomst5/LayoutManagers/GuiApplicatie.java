499d994b3c6507edc0f710db7dfaf73949fbe1a4
ReubenHawley <reubenhawley@protonmail.com>
2022-02-08 11:58:12 +0100

    Bijeenkomst5 GUI opdrachten 1 LayoutManagers g
    - change layout to grid layout
    - remove properties for placement
    - add a hgap and vgap to the grid layout
    - realign the elements to match the assignment

--- src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
+++ src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
@@ -11,7 +11,7 @@ public class GuiApplicatie extends JFrame {
 
     GuiApplicatie() {
         JFrame window = new JFrame();
-        window.setLayout(new BorderLayout());
+        window.setLayout(new GridLayout(2,3,5,10));
         window.setBounds(0, 0, 400, 400);
         window.setTitle("GuiApplicatie");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,34 +19,34 @@ public class GuiApplicatie extends JFrame {
 
         Dimension d = new Dimension(50, 50);
 
-        JLabel noord = new JLabel("noord");
-        noord.setOpaque(true);
-        noord.setBackground(Color.GREEN); // import java.awt.Color;
-        window.add(noord,BorderLayout.NORTH);
+        JLabel oost = new JLabel("oost");
+        oost.setOpaque(true);
+        oost.setBackground(Color.RED); // import java.awt.Color;
+        oost.setPreferredSize(d);
+        window.add(oost);
 
         JLabel west = new JLabel("west");
         west.setOpaque(true);
         west.setBackground(Color.CYAN); // import java.awt.Color;
-        window.add(west,BorderLayout.WEST);
+        window.add(west);
 
         JLabel centrum = new JLabel("centrum");
         centrum.setOpaque(true);
         centrum.setBackground(Color.ORANGE); // import java.awt.Color;
         centrum.setPreferredSize(d);
-        window.add(centrum,BorderLayout.CENTER);
+        window.add(centrum);
 
-        JLabel oost = new JLabel("oost");
-        oost.setOpaque(true);
-        oost.setBackground(Color.RED); // import java.awt.Color;
-        oost.setPreferredSize(d);
-        window.add(oost,BorderLayout.EAST);
+        JLabel noord = new JLabel("noord");
+        noord.setOpaque(true);
+        noord.setBackground(Color.GREEN); // import java.awt.Color;
+        window.add(noord);
 
         JLabel zuid = new JLabel("zuid");
         zuid.setOpaque(true);
         zuid.setBackground(Color.YELLOW); // import java.awt.Color;
         Dimension southD = new Dimension(400,200);
         zuid.setPreferredSize(southD);
-        window.add(zuid,BorderLayout.SOUTH);
+        window.add(zuid);
 
     }
 }
