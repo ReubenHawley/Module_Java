a20b02ebaf7bef209b9505ffac67007be45f86d4
ReubenHawley <reubenhawley@protonmail.com>
2022-02-08 11:36:53 +0100

    Bijeenkomst5 GUI opdrachten 1 LayoutManagers c
    - create new labels for the other directions
    - set colour
    - set bounds
    - add to window

--- src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
+++ src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
@@ -5,6 +5,9 @@ import java.awt.*;
 
 public class GuiApplicatie extends JFrame {
     private JLabel oost;
+    private JLabel west;
+    private JLabel noord;
+    private JLabel zuid;
 
     GuiApplicatie() {
         JFrame window = new JFrame();
@@ -17,7 +20,25 @@ public class GuiApplicatie extends JFrame {
         JLabel oost = new JLabel("oost");
         oost.setOpaque(true);
         oost.setBackground(Color.RED); // import java.awt.Color;
-        oost.setBounds(10, 20, 30, 80);
+        oost.setBounds(40, 80, 30, 80);
         window.add(oost);
+
+        JLabel west = new JLabel("west");
+        west.setOpaque(true);
+        west.setBackground(Color.CYAN); // import java.awt.Color;
+        west.setBounds(10, 80, 30, 80);
+        window.add(west);
+
+        JLabel noord = new JLabel("noord");
+        noord.setOpaque(true);
+        noord.setBackground(Color.GREEN); // import java.awt.Color;
+        noord.setBounds(10, 20, 60, 80);
+        window.add(noord);
+
+        JLabel zuid = new JLabel("zuid");
+        zuid.setOpaque(true);
+        zuid.setBackground(Color.YELLOW); // import java.awt.Color;
+        zuid.setBounds(10, 160, 60, 80);
+        window.add(zuid);
     }
 }
