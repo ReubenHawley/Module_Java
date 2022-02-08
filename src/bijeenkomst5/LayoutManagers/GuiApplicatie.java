b4035e4e70728d448a1dc8d3d38a53077e92bf53
ReubenHawley <reubenhawley@protonmail.com>
2022-02-08 11:40:11 +0100

    Bijeenkomst5 GUI opdrachten 1 LayoutManagers d
    - change layoutmnager to flow layout(left,0,5)

--- src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
+++ src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
@@ -11,7 +11,7 @@ public class GuiApplicatie extends JFrame {
 
     GuiApplicatie() {
         JFrame window = new JFrame();
-        window.setLayout(null);
+        window.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 5));
         window.setBounds(0, 0, 400, 400);
         window.setTitle("GuiApplicatie");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,25 +20,25 @@ public class GuiApplicatie extends JFrame {
         JLabel oost = new JLabel("oost");
         oost.setOpaque(true);
         oost.setBackground(Color.RED); // import java.awt.Color;
-        oost.setBounds(40, 80, 30, 80);
+//        oost.setBounds(40, 80, 30, 80);
         window.add(oost);
 
         JLabel west = new JLabel("west");
         west.setOpaque(true);
         west.setBackground(Color.CYAN); // import java.awt.Color;
-        west.setBounds(10, 80, 30, 80);
+//        west.setBounds(10, 80, 30, 80);
         window.add(west);
 
         JLabel noord = new JLabel("noord");
         noord.setOpaque(true);
         noord.setBackground(Color.GREEN); // import java.awt.Color;
-        noord.setBounds(10, 20, 60, 80);
+//        noord.setBounds(10, 20, 60, 80);
         window.add(noord);
 
         JLabel zuid = new JLabel("zuid");
         zuid.setOpaque(true);
         zuid.setBackground(Color.YELLOW); // import java.awt.Color;
-        zuid.setBounds(10, 160, 60, 80);
+//        zuid.setBounds(10, 160, 60, 80);
         window.add(zuid);
     }
 }
