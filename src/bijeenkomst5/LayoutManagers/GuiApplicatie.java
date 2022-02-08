0a58472870a63f73c49799827f0dcdc9fcfad45e
ReubenHawley <reubenhawley@protonmail.com>
2022-02-08 11:51:41 +0100

    Bijeenkomst5 GUI opdrachten 1 LayoutManagers f
    - change layout to border layout
    - set dimension for south

--- src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
+++ src/bijeenkomst5/LayoutManagers/GuiApplicatie.java
@@ -11,7 +11,7 @@ public class GuiApplicatie extends JFrame {
 
     GuiApplicatie() {
         JFrame window = new JFrame();
-        window.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 5));
+        window.setLayout(new BorderLayout());
         window.setBounds(0, 0, 400, 400);
         window.setTitle("GuiApplicatie");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,36 +19,34 @@ public class GuiApplicatie extends JFrame {
 
         Dimension d = new Dimension(50, 50);
 
-        JLabel oost = new JLabel("oost");
-        oost.setOpaque(true);
-        oost.setBackground(Color.RED); // import java.awt.Color;
-//        oost.setBounds(40, 80, 30, 80);
-        oost.setPreferredSize(d);
-        window.add(oost);
+        JLabel noord = new JLabel("noord");
+        noord.setOpaque(true);
+        noord.setBackground(Color.GREEN); // import java.awt.Color;
+        window.add(noord,BorderLayout.NORTH);
 
         JLabel west = new JLabel("west");
         west.setOpaque(true);
         west.setBackground(Color.CYAN); // import java.awt.Color;
-//        west.setBounds(10, 80, 30, 80);
-        window.add(west);
+        window.add(west,BorderLayout.WEST);
 
         JLabel centrum = new JLabel("centrum");
         centrum.setOpaque(true);
         centrum.setBackground(Color.ORANGE); // import java.awt.Color;
-//        oost.setBounds(40, 80, 30, 80);
         centrum.setPreferredSize(d);
-        window.add(centrum);
+        window.add(centrum,BorderLayout.CENTER);
 
-        JLabel noord = new JLabel("noord");
-        noord.setOpaque(true);
-        noord.setBackground(Color.GREEN); // import java.awt.Color;
-//        noord.setBounds(10, 20, 60, 80);
-        window.add(noord);
+        JLabel oost = new JLabel("oost");
+        oost.setOpaque(true);
+        oost.setBackground(Color.RED); // import java.awt.Color;
+        oost.setPreferredSize(d);
+        window.add(oost,BorderLayout.EAST);
 
         JLabel zuid = new JLabel("zuid");
         zuid.setOpaque(true);
         zuid.setBackground(Color.YELLOW); // import java.awt.Color;
-//        zuid.setBounds(10, 160, 60, 80);
-        window.add(zuid);
+        Dimension southD = new Dimension(400,200);
+        zuid.setPreferredSize(southD);
+        window.add(zuid,BorderLayout.SOUTH);
+
     }
 }
